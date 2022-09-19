9489a891631e394e2a6b487602b7943660825845
Oluviktor <femtorvv@gmail.com>
2022-09-18 19:02:53 -0700

    Numbers task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success/correct)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			for (i = 0; i < 10; i++)
+
+					{
+
+								printf("%d", i);
+
+									}
+
+				printf("\n");
+
+					return (0);
+
+}
