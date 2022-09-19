8b707ee43f1f59fe4571a23d3835e57ed31ed204
Oluviktor <femtorvv@gmail.com>
2022-09-18 19:58:25 -0700

    _putchar
     task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,47 @@
+
+
 #include "main.h"
 
+
+
 /**
- * main - prints _putchar
  *
- * Description: prints _putchar
+ * *main - Entry point
  *
- * Return: 0 (Success)
- */
+ * *
+ *
+ * *Return: 0 nothing, return void
+ *
+ * */
+
+
 
 int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
 
-return (0);
+
+
+{
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+
+
+											return (0);
+
 }
