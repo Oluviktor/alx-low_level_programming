c0f1b38dd3f7ee9ea70db57f6ec279ee6ad876c4
Oluviktor <femtorvv@gmail.com>
2022-09-19 21:40:42 -0700

    I do not fear computers task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _puts - prints a string follow by a new line..
+ *  @str: the string to print.
+ *  Return: void
+*/
+
+void _puts(char *str)
+{
+	int num;
+
+	for (num = 0; str[num] != '\0'; num++)
+	{
+		_putchar(str[num]);
+	}
+	_putchar('\n');
+}
