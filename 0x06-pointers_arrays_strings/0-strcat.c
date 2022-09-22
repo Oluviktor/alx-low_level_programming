bd0120a431562fcabcc1705f098a414e228d2409
Oluviktor <femtorvv@gmail.com>
2022-09-21 18:42:53 -0700

    strcat task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-int index = 0, dest_len = 0;
+int i = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
 
 	return (dest);
 }
