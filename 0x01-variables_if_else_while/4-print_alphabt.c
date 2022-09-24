3904335dc7c60d1c6f862ff0a7880387f209fb21
Oluviktor <femtorvv@gmail.com>
2022-09-23 17:48:02 -0700

    When I was having that alphabet soup task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success/correct)
- *
- *     */
+*main - Entry point
+*
+*Return: Always 0 (Success/correct)
+*
+*/
 
 int main(void)
 
 {
+char i;
 
-		char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+putchar(i);
 
+}
 
+putchar('\n');
 
-			for (i = 'a'; i <= 'z'; i++)
-
-					{
-
-								if (i != 'q' && i != 'e')
-
-												putchar(i);
-
-									}
-
-				putchar('\n');
-
-					return (0);
+return (0);
 
 }
