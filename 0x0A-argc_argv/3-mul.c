43bedb13b6437e8fe61c97e17a754997e54e0d7b
Oluviktor <femtorvv@gmail.com>
2022-10-01 17:30:33 -0700

    Neither irony nor sacrasm to task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -8,14 +6,16 @@
  * @argv: argument vector
  * Return: 0 if true, 1 if false
  */
+
 int main(int argc, char *argv[])
 {
 	int a, b;
 	if (argc == 3)
+
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-		printf("%d\n", a *b);
+		printf("%d\n", a * b);
 		return (0);
 	}
 	printf("Error\n");
